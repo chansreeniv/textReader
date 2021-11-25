f3f7711c1d80d2e5f69a7c276d06596d5720cdb4
chandan <chansreeniv@yahoo.com>
2021-11-25 13:22:19 +0530

    added json formatting

--- controller/textRead.js
+++ controller/textRead.js
@@ -1,7 +1,14 @@
 const Reader = require('../models/reader');
+const path = require('path');
+const DatabaseObj = require('../models/string2json');
+
+const filePath = path.join(path.dirname(require.main.filename), 'data', 'textread.txt');
 
 exports.getParsedText = (req, res, next) =>{
-    res.render('index');
-    const reader = new Reader;
-    reader.textParser();
+    const reader = new Reader(filePath);
+    reader.textParser((result)=>{
+        res.render('index',{response: result}); 
+        const databaseObj = new DatabaseObj(result);
+        databaseObj.string2json();
+    });
 }
--- models/reader.js
+++ models/reader.js
@@ -1,47 +1,49 @@
-const fs = require('fs');
-const txt = require('txt');
-const pdf = require('pdf-parse');
-const path = require('path');
+const fs = require("fs");
+const txt = require("txt");
+// const pdf = require('pdf-parse');
 
-const p = path.join(path.dirname(require.main.filename), 'data', 'textread.txt');
+module.exports = class Reader {
+  constructor(filePath) {
+    this.filePath = filePath;
+  }
 
-module.exports = class Reader{
-    constructor(){
-    }
+  textParser(cb) {
+    fs.readFile(this.filePath, "utf8", (err, datafile) => {
+      const textReader = this.searchText(datafile);
+      this.cleanedText = textReader.replace(
+        /CR No.:\[|Name:\[| Ph:\[| \| |Age:\[/gi,
+        " "
+      );
+      console.log(this.cleanedText + 'cleaned text');
+      cb(this.cleanedText);
+    });
 
-    textParser(){
-        fs.readFile(p,'utf8', (err, datafile) =>{
-           const textReader = this.searchText(datafile);
-           let cleanedText = textReader.replace(/CR No.:\[|Name:\[| Ph:\[| \| |Age:\[/gi," ")
-           console.log(cleanedText);
-        })
+    // fs.readFile(p, (err, datafile)=>{
+    //     this.pdf2txt(datafile);
+    // })
+    return this.cleanedText
+  }
 
-        // fs.readFile(p, (err, datafile)=>{
-        //     this.pdf2txt(datafile);
-        // })
-    }
+  searchText(data) {
+    // const textMatch = /Date: \[.*/g; //working
+    const textMatch = /CR No.:\[.*/g;
+    let textFile = 0;
+    txt.eachMatch(data, textMatch, (match, detail) => {
+      textFile = match;
+      return match;
+    });
+    return textFile;
+  }
 
-    searchText(data){
+  // pdf2txt(datafile){
+  //     const p = path.join(path.dirname(require.main.filename), 'data', 'CBC.pdf');
+  //     fs.readFile(p, (err, datafile) =>{
+  //         pdf(datafile).
+  //         then((res)=>{
+  //             console.log(res);
+  //         })
+  //     //    console.log(textReader);
+  //     })
 
-        // const textMatch = /Date: \[.*/g; //working 
-        const textMatch = /CR No.:\[.*/g;
-        let textFile = 0; 
-        txt.eachMatch(data, textMatch, (match, detail) => {
-        textFile = match;
-        return match;
-    })
-        return textFile;
-    ;}
-
-    // pdf2txt(datafile){
-    //     const p = path.join(path.dirname(require.main.filename), 'data', 'CBC.pdf');
-    //     fs.readFile(p, (err, datafile) =>{
-    //         pdf(datafile).
-    //         then((res)=>{
-    //             console.log(res);
-    //         })
-    //     //    console.log(textReader);
-    //     })
-        
-    // }
-}
+  // }
+};
